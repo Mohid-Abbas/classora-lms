d2c00b6c1ffaf1783da99a2589ca2f2bd3cf679d
Mohid-Abbas <mohidabbas1001@gmail.com>
2026-03-08 21:53:24 +0500

    Create serializers.py

---
+++ lms/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Course, Lecture, Assignment, Quiz, Question, AttendanceRecord, AttendanceEntry, Announcement
+from accounts.serializers import UserSerializer
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+class LectureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = "__all__"
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = "__all__"
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = "__all__"
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+class AttendanceEntrySerializer(serializers.ModelSerializer):
+    student_name = serializers.ReadOnlyField(source="student.full_name")
+    class Meta:
+        model = AttendanceEntry
+        fields = "__all__"
+
+class AttendanceRecordSerializer(serializers.ModelSerializer):
+    entries = AttendanceEntrySerializer(many=True, read_only=True)
+    class Meta:
+        model = AttendanceRecord
+        fields = "__all__"
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+    author_name = serializers.ReadOnlyField(source="author.full_name")
+    class Meta:
+        model = Announcement
+        fields = "__all__"
